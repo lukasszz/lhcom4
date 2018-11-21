f4cf2f4c39dd4ae18e999da93492ea475a26d0a2
lukasz <lukasz>
2018-11-21 22:17:54 +0100

    quantum dash

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,3 +1,4 @@
+import pickle
 from math import ceil
 
 from flask import render_template, g, redirect, request, url_for, current_app
@@ -19,9 +20,19 @@ def index():
     return render_template('index.html', title='Home', user=current_user)
 
 
+@bp.route('/dash')
+def dash():
+    f = open('/tmp/ibm_backends.pickle', 'rb')
+    dr = pickle.load(f)
+    print(dr['tms'])
+    print(dr['backends'])
+
+    return render_template('dash.html', title='Dash', user=current_user, data=dr)
+
+
 @bp.route('/search')
 def search():
-    per_page =15
+    per_page = 15
     if not g.search_form.validate():
         return redirect(url_for('main.explore'))
     page = request.args.get('page', 1, type=int)
@@ -32,4 +43,4 @@ def search():
     prev_url = url_for('main.search', q=g.search_form.q.data, page=page - 1) \
         if page > 1 else None
     return render_template('search.html', title='Search', jrnls=jrnls,
-                           next_url=next_url, prev_url=prev_url, page=page, total=ceil(total/per_page))
+                           next_url=next_url, prev_url=prev_url, page=page, total=ceil(total / per_page))
