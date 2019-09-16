4661e539a8e6b08e33823631e61316a229b7088d
lukasz <lukasz.herok@gmail.com>
2019-09-16 09:09:51 +0200

    Bbdays4it

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 export FLASK_APP=microblog.py
 export FLASK_DEBUG=1
-sudo systemctl start elasticsearch
+#sudo systemctl start elasticsearch
+echo "Notice: Without Elastic search"
 flask run
 
